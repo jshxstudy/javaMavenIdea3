8b03dcf64ad63137517cde17008594bf4c0d5be9
jinshangxiong <jinshangxiong@emar.com>
2019-12-23 13:06:15 +0800

    第四次提交

--- javaMavenIdea3Module1/src/main/java/org/jshx/test/module1/App.java
+++ javaMavenIdea3Module1/src/main/java/org/jshx/test/module1/App.java
@@ -2,7 +2,7 @@ package org.jshx.test.module1;
 
 public class App {
     public static void main(String[] args) {
-        Person p = new Person("张三");
+        Person p = new Person("张三1");
         System.out.println(p.getName());
     }
 }
