53195d10916ccda6f65d2ed23d68bac67cd4af49
jinshangxiong <jinshangxiong@emar.com>
2019-12-23 12:48:12 +0800

    第三次提交

---
+++ javaMavenIdea3Module1/src/main/java/org/jshx/test/module1/App.java
@@ -0,0 +1,8 @@
+package org.jshx.test.module1;
+
+public class App {
+    public static void main(String[] args) {
+        Person p = new Person("张三");
+        System.out.println(p.getName());
+    }
+}
